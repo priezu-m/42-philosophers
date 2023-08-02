6bdfa1cb8e4d03cffcd175cfd591d530d695c7a1
Peru Riezu Muñoz <peru@riezu.eus>
2023-08-02 18:02:41 +0000

    began testing

---
+++ test.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   Filename: test.c                                                         */
+/*   Author:   Peru Riezu <riezumunozperu@gmail.com>                          */
+/*   github:   https://github.com/priezu-m                                    */
+/*   Licence:  GPLv3                                                          */
+/*   Created:  2023/08/02 18:02:30                                            */
+/*   Updated:  2023/08/02 18:02:32                                            */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma GCC diagnostic warning "-Weverything"
+#pragma GCC diagnostic ignored "-Wempty-translation-unit"
+
+
