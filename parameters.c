d16112a46ebac7a073c5424c162377d2faa44cc3
Peru Riezu <riezumunozperu@gmail.com>
2023-09-16 14:47:11 +0200

    proper redaction of usage message

--- parameters.c
+++ parameters.c
@@ -5,10 +5,14 @@
 /*   Author:   Peru Riezu <riezumunozperu@gmail.com>                          */
 /*   github:   https://github.com/priezu-m                                    */
 /*   Licence:  GPLv3                                                          */
-/*   Created:  2023/09/15 20:24:28                                            */
-/*   Updated:  2023/09/16 01:01:11                                            */
-/*                                                                            */ 
+/*   Created:  2023/09/16 14:24:48                                            */
+/*   Updated:  2023/09/16 14:46:13                                            */
+/*                                                                            */
 /* ************************************************************************** */
+
+#include "parameters.h"
+#include <limits.h>
+#include <stdio.h>
 
 ;
 #pragma clang diagnostic push
@@ -16,36 +20,45 @@
 #pragma clang diagnostic ignored "-Wempty-translation-unit"
 #pragma clang diagnostic ignored "-Wunused-macros"
 
-#include "parameters.h"
-#include <limits.h>
-#include <stdio.h>
+static void	print_usage_part_one(void)
+{
+	printf(
+		"Usage: ./philo NUMBER_OF_PHILOSOPHERS TIME_TO_STARVE TIME_EATING\n"
+		"\tTIME_SLEEPING [NUMBER_OF_MEALS_EACH_PHILOSOPHER_MUST_EAT]\n\n"
+		"Simulate NUMBER_OF_PHILOSOPHERS philosophers, with each philosopher\n"
+		"in a round table alternating between eating, sleeping, and thinking.\n"
+		"At the beginning of the simulation, each philosopher has a fork on\n"
+		"their right. Since the table is round, if at least two philosophers\n"
+		"are sitting, each philosopher starts with a fork on their right and\n"
+		"left. Philosophers need to take two forks before they can begin\n"
+		"eating. Philosophers can only take the forks to their right and\n"
+		"left and must return them to their original positions after they\n"
+		"finish eating. Philosophers eat for TIME_EATING milliseconds,\n"
+		"then sleep for TIME_SLEEPING milliseconds, and finally, they\n"
+		"think until they can eat again. Philosophers are unaware of the\n"
+		"state (eating, sleeping, thinking) of the other philosophers. If\n"
+		"TIME_TO_STARVE milliseconds pass since a philosopher last started\n"
+		"eating (or since the simulation began), the philosopher dies, and the\n"
+		);
+}
 
-static char	*usage = 
-	"Usage: ./philo NUMBER_OF_PHILOSOPHERS TIME_TO_STARVE TIME_EATING"
-	"\tTIME_SLEEPING [NUMBER_OF_MEALS_EACH_PHILOSOPHER_MUST_EAT]\n\n"
-	"Simulate NUMBER_OF_PHILOSOPHERS philosophers, with each philosopher"
-	"in a round table alternating between eating, sleeping, and thinking."
-	"At the beginning of the simulation, each philosopher has a fork on their"
-	"right. Since the table is round, if at least two philosophers are sitting,"
-	"each philosopher starts with a fork on their right and left. Philosophers"
-	"need to take two forks before they can begin eating. Philosophers can only"
-	"take the forks to their right and left and must return them to their"
-	"original positions after they finish eating. Philosophers eat for"
-	"TIME_EATING milliseconds, then sleep for TIME_SLEEPING milliseconds, and"
-	"finally, they think until they can eat again. Philosophers are unaware"
-	"of the state (eating, sleeping, thinking) of the other philosophers."
-	"If TIME_TO_STARVE milliseconds pass since a philosopher last started"
-	"eating (or since the simulation began), the philosopher dies, and the"
-	"simulation ends. If all philosophers have eaten"
-	"NUMBER_OF_MEALS_EACH_PHILOSOPHER_MUST_EAT meals, the simulation ends. If"
-	"this argument is not specified, the simulation will run until a "
-	"philosopher dies.\n\n"
-	"Note: All parameters must be strings of digits forming a number less than"
-	"2^32. NUMBER_OF_PHILOSOPHERS must be at least one.";
+static void	print_usage_part_two(void)
+{
+	printf(
+		"simulation ends. If all philosophers have eaten\n"
+		"NUMBER_OF_MEALS_EACH_PHILOSOPHER_MUST_EAT meals, the simulation\n"
+		"ends. If this argument is not specified, the simulation will run\n"
+		"until a philosopher dies. Each philosopher simulated is a different\n"
+		"thread of execution\n\n"
+		"Note: All parameters must be strings of digits forming a number\n"
+		"less than 2^32. And NUMBER_OF_PHILOSOPHERS must be at least one.\n"
+		);
+}
 
 void	print_usage(void)
 {
-
+	print_usage_part_one();
+	print_usage_part_two();
 }
 
 static int	trunc_atoi(char *str)
@@ -83,7 +96,7 @@ t_parameters	get_parameters(int argc, char **argv)
 	if (argc == 6)
 		parameters.number_of_meals_needed = trunc_atoi(argv[5]);
 	else
-		parameters.number_of_meals_needed = - 1;
+		parameters.number_of_meals_needed = -1;
 	if (parameters.number_of_philosophers < 1
 		|| parameters.time_to_starve == INT_MIN
 		|| parameters.time_eating == INT_MIN
